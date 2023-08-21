57b09bd5e4d9611d81e970a3effa08bd49a7aa3b
Taegwan Youn <tkyoun0421@naver.com>
2023-08-21 15:49:52 +0900

    Feat: 기본 스타일 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    render() {
+        return (
+            <main className="main-container">
+                <h1>예산 계산기</h1>
+                <div
+                    style={{
+                        width: '100%',
+                        backgroundColor: 'white',
+                        padding: '1rem',
+                    }}
+                >
+                    Expense Form
+                </div>
+                <div
+                    style={{
+                        width: '100%',
+                        backgroundColor: 'white',
+                        padding: '1rem',
+                    }}
+                >
+                    Expense List
+                </div>
+                <div
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'end',
+                        marginTop: '1rem',
+                    }}
+                >
+                    <p style={{ fontSize: '2rem' }}>
+                        총지출:
+                        <span>원</span>
+                    </p>
+                </div>
+            </main>
+        );
+    }
 }
-
-export default App;
