36c5c189fdc32c5f2fec3ea35a46e07e4c026a8f
태관 <tkyoun0421@naver.com>
2023-08-22 16:47:28 +0900

    Refactor: 클래스형 컴포넌트 함수형으로 변환

--- src/App.js
+++ src/App.js
@@ -1,51 +1,56 @@
 import './App.css';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import ExpenseForm from './components/ExpenseForm';
 import ExpenseList from './components/ExpenseList';
 
-export default class App extends Component {
 
-    initialExpenses = [
+const App = () => {
+    const [expenses, setExpenses] = useState([
         {id: 1, charge: '렌트비', amount: 1600},
         {id: 2, charge: '교통비', amount: 400},
         {id: 3, charge: '식사', amount: 1200}
-    ]
+    ]);
 
-    render() {
-        return (
-            <main className="main-container">
-                <h1>예산 계산기</h1>
-                <div
-                    style={{
-                        width: '100%',
-                        backgroundColor: 'white',
-                        padding: '1rem',
-                    }}
-                >
-                    <ExpenseForm />
-                </div>
-                <div
-                    style={{
-                        width: '100%',
-                        backgroundColor: 'white',
-                        padding: '1rem',
-                    }}
-                >
-                    <ExpenseList initialExpenses={this.initialExpenses}/>
-                </div>
-                <div
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'end',
-                        marginTop: '1rem',
-                    }}
-                >
-                    <p style={{ fontSize: '2rem' }}>
-                        총지출:
-                        <span>원</span>
-                    </p>
-                </div>
-            </main>
-        );
+    const handleDelete = (id) => {
+        const newExpenses = expenses.filter(expense => expense.id !== id );
+        setExpenses(newExpenses);
     }
+
+    return (
+        <main className="main-container">
+            <h1>예산 계산기</h1>
+            <div
+                style={{
+                    width: '100%',
+                    backgroundColor: 'white',
+                    padding: '1rem',
+                }}
+            >
+                <ExpenseForm />
+            </div>
+            <div
+                style={{
+                    width: '100%',
+                    backgroundColor: 'white',
+                    padding: '1rem',
+                }}
+            >
+                <ExpenseList expenses={expenses} handleDelete={handleDelete}/>
+            </div>
+            <div
+                style={{
+                    display: 'flex',
+                    justifyContent: 'end',
+                    marginTop: '1rem',
+                }}
+            >
+                <p style={{ fontSize: '2rem' }}>
+                    총지출:
+                    <span>원</span>
+                </p>
+            </div>
+        </main>
+    );
 }
+
+export default App
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './ExpenseList.css';
 import ExpenseItem from './ExpenseItem';
 
-export default class ExpenseList extends Component {
-    render() {
-        return (
-            <>
-                <ul className="list">
-                    {this.props.initialExpenses.map(expense => {
-                        return <ExpenseItem expense = {expense} key={expense.id}/>
-                    })}
-                </ul>
-                <button className="btn">목록 지우기</button>
-            </>
-        );
-    }
+
+const ExpenseList = ({expenses, handleDelete}) => {
+    return (
+        <>
+            <ul className="list">
+                {expenses.map(expense => {
+                    return <ExpenseItem expense = {expense} key={expense.id} handleDelete={handleDelete}/>
+                })}
+            </ul>
+            <button className="btn">목록 지우기</button>
+        </>
+    );
 }
+
+export default ExpenseList
